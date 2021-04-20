20a472a2add91b9df97537cf4dd1294ba7beb843
evdwarf <eimer.aru1127@gmail.com>
2021-04-20 19:15:23 +0900

    Update deck card setting of player board

---
+++ Sources/PTCGPlayerBoard/PTCGDeckSet.swift
@@ -0,0 +1,24 @@
+//
+//  PTCGDeckSet.swift
+//  PTCGPlayerBoard
+//
+//  Created by Eimer on 2021/04/19.
+//
+
+import Foundation
+import PTCGCard
+
+public typealias PTCGDeckCardIdentifier = String
+private let PTCGDeckCardIdentifierSet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    .map(PTCGDeckCardIdentifier.init)
+
+public struct PTCGDeckSet {
+
+    public let cards: Array<PTCGDeckCard>
+    
+    public init(cards: Array<AnyPTCGCard>) {
+        self.cards = cards.enumerated().map {
+            PTCGDeckCard(with: $0.element, PTCGDeckCardIdentifierSet[$0.offset])
+        }
+    }
+}
--- Sources/PTCGPlayerBoard/PTCGPlayerBoard.swift
+++ Sources/PTCGPlayerBoard/PTCGPlayerBoard.swift
@@ -1,17 +1,90 @@
-import PTCGBattlePokemon
 import PTCGCard
 
-struct PTCGPlayerBoard {
-    var text = "Hello, World!"
+/**
+ * ポケモンカードのゲーム開始時に用意する初期手札枚数
+ */
+public let PTCGStartHandsCount = 7
+
+/**
+ * ポケモンカードの初期サイドカード枚数
+ */
+public let PTCGDefaultPrizeCount = 6
+
+/**
+ * ポケモンカードゲームのプレイヤー毎の盤面
+ */
+public struct PTCGPlayerBoard: PTCGZoneControllable {
     
-    func sandbox() -> PTCGBattlePokemon {
-        let bp = PTCGBattlePokemon.init(with: PTCGPokemonCard.init(
-                                            id: "",
-                                            name: "",
-                                            type: .colorLess,
-                                            maxHitPoint: 0,
-                                            evolutionType: .basic,
-                                            ability: nil))
-        return bp
+    // MARK: Initialize
+    
+    /**
+     * ゲーム開始上の手札枚数
+     */
+    public let startHandsCount: Int
+    
+    /**
+     * ゲーム開始上のサイド枚数
+     */
+    public let gamePrizeCount: Int
+    
+    /**
+     * ゲームで使用するデッキセット
+     */
+    public let deckSet: PTCGDeckSet
+    
+    /**
+     * 盤面を生成
+     * - Parameters:
+     *   - hands: 手札の初期枚数
+     *   - prize: サイドの枚数
+     *   - deckSet: 使用するデッキセット
+     */
+    public init(
+        hands: Int = PTCGStartHandsCount,
+        prize: Int = PTCGDefaultPrizeCount,
+        deckSet: PTCGDeckSet)
+    {
+        self.startHandsCount = hands
+        self.gamePrizeCount = prize
+        self.deckSet = deckSet
+    }
+    
+    // MARK: PTCGZoneControllable
+    
+    /** デッキ */
+    public var deck         : PTCGDeckZone          = .init()
+    /** バトル場 */
+    public var battleActive : PTCGBattleActiveZone  = .init()
+    /** ベンチ */
+    public var battleBench  : PTCGBattleBenchZone   = .init()
+    /** スタジアム */
+    public var stadium      : PTCGStadiumZone       = .init()
+    /** 手札 */
+    public var hands        : PTCGHandsZone         = .init()
+    /** トラッシュ */
+    public var discardPile  : PTCGDiscardPileZone   = .init()
+    /** サイド */
+    public var prize        : PTCGPrizeZone         = .init()
+}
+
+public extension PTCGPlayerBoard {
+    
+    enum Abc {
+        case hoge
+    }
+    
+    /**
+     * カードの挿入位置
+     */
+    enum InsertPosition {
+        /**
+         * カードの山の一番上に挿入
+         */
+        case on
+
+        /**
+         * カードの山の一番下に挿入
+         */
+        case under
     }
 }
